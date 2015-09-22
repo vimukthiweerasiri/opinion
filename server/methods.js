249c8c3eecc8567e58e47b2561190b797ce53db5
Vimukthi Weerasiri <vimukthiweerasiri@gmail.com>
2015-09-22 10:33:16 +0530

    stable

--- server/methods.js
+++ server/methods.js
@@ -1,27 +1,6 @@
 Meteor.methods({
-    'test': function(){
-        console.log("CALLED");
-        return Meteor.settings.twitterKey;
-    },
-    'test2': function(){
-        console.log("CALLED");
-        return 'second workingkjkkjk';
-    },
     'analyze': function (name) {
-        //this.unblock();
-        //var ttt = APP.analyze(name);
-        //console.log(ttt);
-        //return ttt;
-        return abc();
+        this.unblock();
+        return APP.analyze(name);
     }
 });
-
-var abc = function () {
-    var ttt = function (callback) {
-        setTimeout(function () {
-            callback(null, {negative: 4, neutral:5+ (Math.random() * 10), positive:8});
-        }, 5000);
-    }
-    var TTT = Meteor.wrapAsync(ttt);
-    return TTT();
-}
